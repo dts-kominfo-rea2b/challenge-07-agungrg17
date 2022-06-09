d1916242397c631c6805ae870c45a86b26ec6185
Agung Rakhmat Gumilar <agungrg17@gmail.com>
2022-06-09 22:10:39 +0700

    edit promise.js, buat fungsi promiseOutput yg menghitung length hasil

--- promise.js
+++ promise.js
@@ -1,7 +1,18 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const theaterIxx = await promiseTheaterIXX();
+  const theaterVgc = await promiseTheaterVGC();
+  
+  const emosiTheaterIxx = theaterIxx.filter((element) => {
+    return element.hasil === emosi;
+  });
+  const emosiTheaterVgc = theaterVgc.filter((element) => {
+    return element.hasil === emosi;
+  });
+    return emosiTheaterIxx.length + emosiTheaterVgc.length;
+};
 
 module.exports = {
   promiseOutput,
